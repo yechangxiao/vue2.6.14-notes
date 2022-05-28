03a01b97bcb16f48290afa6b70e5002ef1236929
yechangxiao <yechangxiaoycx@qq.com>
2022-05-28 10:37:38 +0800

    [doc]: 添加Vue.component/filter/directive的注释

--- src/core/global-api/assets.js
+++ src/core/global-api/assets.js
@@ -7,25 +7,35 @@ export function initAssetRegisters (Vue: GlobalAPI) {
   /**
    * Create asset registration methods.
    */
+  // ASSET_TYPES包括component、directive、filter
+  // 为Vue定义对应方法
   ASSET_TYPES.forEach(type => {
     Vue[type] = function (
       id: string,
       definition: Function | Object
     ): Function | Object | void {
       if (!definition) {
+        // 获取对应的组件、指令、过滤器
         return this.options[type + 's'][id]
       } else {
         /* istanbul ignore if */
         if (process.env.NODE_ENV !== 'production' && type === 'component') {
           validateComponentName(id)
         }
+        // 判断是否是原始的Object对象
+        // 通过Object.prototype.toString.call(obj) === '[object Object]'
         if (type === 'component' && isPlainObject(definition)) {
           definition.name = definition.name || id
+          // this.options._base里面保存的就是Vue构造函数
+          // 把组件配置转换为组件的构造函数
           definition = this.options._base.extend(definition)
         }
         if (type === 'directive' && typeof definition === 'function') {
           definition = { bind: definition, update: definition }
         }
+        // this就是Vue，进行全局注册，存储资源并赋值
+        // 如果传入对象，则直接进行存储并赋值
+        // this.options['components']['comp'] = definition
         this.options[type + 's'][id] = definition
         return definition
       }
